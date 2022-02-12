783aeb72ef2aa0d61b70245c05a4d0ef447d7fed
Matteus247 <matei.tanase19@imperial.ac.uk>
2022-02-12 02:36:40 +0000

    Identified one way to calibrate gyro, added data recording

--- 9DOF_interfacing/gyro_test.py
+++ 9DOF_interfacing/gyro_test.py
@@ -15,6 +15,7 @@ GYRO_RANGE_500DPS = 500 #,   /**< 500dps */
 GYRO_SENSITIVITY_250DPS = 0.0078125 #lower dynamic range -> slow rotations
 GYRO_SENSITIVITY_500DPS = 0.015625 
 GYRO_SENSITIVITY_1000DPS= 0.03125 #broader dynamic range -> fast rotations
+GYRO_SENSITIVITY_2000DPS= 0.0625
 GYRO_DPS_TO_RADS = 0.017453293
 
 with smbus2.SMBus(1) as bus:
@@ -24,7 +25,7 @@ with smbus2.SMBus(1) as bus:
     time.sleep(0.1)
     #bus.write_byte_data(0x21, 0x13, 0x40)
     time.sleep(0.1)
-    bus.write_byte_data(0x21, 0x0D, 0x03)
+    bus.write_byte_data(0x21, 0x0D, 0x02) #change gyro sensitivity
     time.sleep(0.1)
     bus.write_byte_data(0x21, 0x13, 0x0E)
     time.sleep(0.1)
@@ -34,10 +35,15 @@ with smbus2.SMBus(1) as bus:
     #time.sleep(0.1)
     
     max_x = 0
+    min_x = 50
     max_y = 0
     max_z = 0
+    min_z = 50
     
-    while True:
+    readings = open("readings_z_axis.txt", "w")
+    readings.write("line\n")
+    i = 0 
+    while i < 100:
         Buffer = bus.read_i2c_block_data(0x21, 0x00, 7)
         raw_gyro_x = ((Buffer[1] << 8) | Buffer[2])
         raw_gyro_y = ((Buffer[3] << 8) | Buffer[4])
@@ -48,12 +54,15 @@ with smbus2.SMBus(1) as bus:
         gyro_z = raw_gyro_z * GYRO_SENSITIVITY_1000DPS * GYRO_DPS_TO_RADS
         
         #implement wrap around
+        '''
         if gyro_x > max_x:
             max_x = gyro_x
         if gyro_y > max_y:
             max_y = gyro_y
         if gyro_z > max_z:
             max_z = gyro_z
+        '''
+        
         '''   
         if gyro_x > 4.467: #FOR 250 DPS
             gyro_x = gyro_x - 8.9359
@@ -73,8 +82,30 @@ with smbus2.SMBus(1) as bus:
         #max is 35.7436 for 1000 DPS
             
         #fine calibration TBD, need an Arduino for reference
+        if gyro_x > max_x:
+            max_x = gyro_x
+        if gyro_y > max_y:
+            max_y = gyro_y
+        if gyro_z > max_z:
+            max_z = gyro_z
+        if gyro_x < min_x:
+            min_x = gyro_x
+        if gyro_z < min_z:
+            min_z = gyro_z
         
         print("X: ", gyro_x, "\tY: ", gyro_y, "\tZ: ", gyro_z)
+        
+        #print(min_x, "\t", max_x)
+        print(min_z, "\t", max_z)
+        secs = time.time()
+        
+        readings.write(str(gyro_z))
+        readings.write(",")
+        readings.write(str(secs))
+        readings.write("\n")
+        '''
+        readings.write(str(max_z))
+        readings.write("\n")'''
         
         '''
         print(max_x)
@@ -83,6 +114,8 @@ with smbus2.SMBus(1) as bus:
         '''
         
         print()
+        i = i + 1
         time.sleep(0.3)
             
     
+    readings.close()
