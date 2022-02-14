eee281dbb25ed0f497b34ec89764c1eaf03f21cf
Miles Grist <milesgrist@gmail.com>
2022-02-14 14:15:00 +0000

    refactored. Re-arranged how signals are stored in log to make json object simpler. Added button and leds for testing.

--- RPi_Final/Log.py
+++ RPi_Final/Log.py
@@ -7,15 +7,17 @@ import time
 class Log:
     def __init__(self):
         self.buffer_length = 5000  # Number of data points stored
-        self.a_points = [[], []]  # Sets of data points for each sensor
+        self.buffers = [[[], []], [[], []]]  # data for each sensor [[[data, ...],[time, ...]], ...]
         for i in range(self.buffer_length):  #
-            self.a_points[0].append([0, 0])
-            self.a_points[1].append([0, 0])
-        self.a_i = [0, 0]
+            self.buffers[0][0].append(0)
+            self.buffers[0][1].append(0)
+            self.buffers[1][0].append(0)
+            self.buffers[1][1].append(0)
+        self.buffer_i = [0, 0]
         self.events = []
         self.event_i = 0  # Index of next event to be sent via MQTT
-        self.a_offsets = [1430, 1430]
-        self.a_cal = [0, 0]
+        self.sig_offsets = [1430, 1430]  # Removes DC offset read at the ADC
+        self.sig_cal = [0, 0]  # Based upon the user's resting, balanced weight
 
     def i_wrap(self, i):
         if i < (self.buffer_length - 1):
@@ -24,37 +26,46 @@ class Log:
             return 0
 
     def i_convert(self, i_relative, i_current):  # Maps relative index to current index in a buffer
-        if i_relative < 0:  # Useful when applying lpf to not wrap the filter
-            return i_current
         i = i_current + i_relative
         if i > self.buffer_length - 1:
             return i - self.buffer_length
+        elif i < 0:
+            return self.buffer_length + i
         else:
             return i
 
-    def lpf(self, sig_id, width):  # Time averaging of defined width
-        out = []
-        for i in range(len(self.a_points[sig_id])):
-            d_sum = 0
-            for c in range(width):
-                d_sum += self.a_points[sig_id][self.i_convert(i + c - width + 1, self.a_i[sig_id])][0]
-            t_avg = int(d_sum/width)
-            out.append([t_avg, self.a_points[sig_id][self.i_convert(i, self.a_i[sig_id])][1]])
-        return out
+    def lpf(self, sig_id, index, width):  # Time averaging of defined width, at a single index
+        d_sum = 0
+        for c in range(width):
+            d_sum += self.buffers[sig_id][0][self.i_convert(index + c - width + 1, self.buffer_i[sig_id])]
+        return int(d_sum / width)
+
+    def rolling_lpf(self, sig_id, width):
+        return 0
+
+    def lpf_buffer(self, sig_id, width):  # Time averaging of defined width, entire buffer
+        filtered_buffer = [[], []]
+        for i in range(self.buffer_length):
+            filtered_buffer[0].append(self.lpf(sig_id, -i, width))
+            filtered_buffer[1].append(self.buffers[sig_id][1][self.i_convert(-i, self.buffer_i[sig_id] - 1)])
+        return filtered_buffer  # new buffer, arranged in time order
 
     def write(self, sig_id, d):  # Write a datapoint into a buffer and timestamp it
-        self.a_points[sig_id][self.a_i[sig_id]] = [d, time.process_time()]
-        self.a_i[sig_id] = self.i_wrap(self.a_i[sig_id])
+        self.buffers[sig_id][0][self.buffer_i[sig_id]] = d
+        self.buffers[sig_id][1][self.buffer_i[sig_id]] = time.process_time()
+        self.buffer_i[sig_id] = self.i_wrap(self.buffer_i[sig_id])  # Increment buffer index
 
     def save_event(self):  # Formats the raw sensor data lpf -> offset -> calibration -> adjust timestamps to start at 0
-        a0 = self.lpf(0, 4)
-        a1 = self.lpf(1, 10)
-        t_off0 = a0[0][1]
-        t_off1 = a1[0][1]
-        for i in range(len(a0)):
-            a0[i][0] = a0[i][0] - self.a_offsets[0]
-            a0[i][1] = round(a0[i][1] - t_off0, 4)  # Set the time of the event to start from 0
-        for i in range(len(a1)):
-            a1[i][0] = a1[i][0] - self.a_offsets[1]
-            a1[i][1] = round(a1[i][1] - t_off1, 4)
+        print("saving event")
+        a0 = self.lpf_buffer(0, 5)
+        #print(a0)
+        a1 = self.lpf_buffer(1, 10)
+        t_off0 = a0[1][self.buffer_length - 1]
+        t_off1 = a1[1][0]
+        for i in range(self.buffer_length):
+            a0[0][i] = a0[0][i] - self.sig_offsets[0]
+            a0[1][i] = round(a0[1][i] - t_off0, 4)  # Set the time of the event to start from 0
+        for i in range(self.buffer_length):
+            a1[0][1] = a1[0][i] - self.sig_offsets[1]
+            a1[1][i] = round(a1[1][i] - t_off1, 4)
         self.events.append([a0, a1])
--- RPi_Final/main.py
+++ RPi_Final/main.py
@@ -6,6 +6,7 @@ from Log import Log
 from FsrAdc import FsrAdc
 import concurrent.futures
 from gpiozero import Button
+from gpiozero import LED
 
 log = Log()
 
@@ -14,34 +15,33 @@ adc1 = FsrAdc(0x49, 17)
 
 t0 = time.process_time()
 
+cal_button = Button(27)
+red_led = LED(23)
+green_led = LED(22)
+
+
 def readFsrAdcs():
-    while (time.process_time() - t0 < 15):
+    while (time.process_time() - t0 < 20):
         if not adc0.alert.is_held:
             log.write(0, adc0.read_conversion())
         if not adc1.alert.is_held:
             log.write(1, adc1.read_conversion())
 
+
 def eventLogging():
-    time.sleep(20)
+    red_led.on()
+    green_led.off()
+    cal_button.wait_for_active()
+    red_led.blink(0.1, 0.1)
+    green_led.on()
+
+    time.sleep(15)
     log.save_event()
 
-    with open('data_out.txt', 'w') as f:  # Write out data
-        for point in log.a_points[0]:
-            f.write(str(point[0]) + ", " + str(point[1]))
-            f.write('\n')
-    with open('data_out1.txt', 'w') as f:
-        for point in log.a_points[1]:
-            f.write(str(point[0]) + ", " + str(point[1]))
-            f.write('\n')
     with open('proc_out.txt', 'w') as f:
-        for point in log.events[0][0]:
-            f.write(str(point[0]) + ", " + str(point[1]))
+        for i in range(5000):
+            f.write(str(log.events[0][0][0][i]) + ", " + str(log.events[0][0][1][i]))
             f.write('\n')
-    with open('proc_out1.txt', 'w') as f:
-        for point in log.events[0][1]:
-            f.write(str(point[0]) + ", " + str(point[1]))
-            f.write('\n')
-
 
 if __name__ == '__main__':
     threads = []
